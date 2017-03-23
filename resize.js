804d9b7fcc5da0f04948c24b4d38a96ba4b3defb
sphsconormurphy <8cmurphy@stpatrick.org>
2017-03-23 13:54:53 -0500

    Day 3

--- resize.js
+++ resize.js
@@ -1,4 +1,13 @@
 //Declare tabletSize() here
-
+function tabletSize(){
+  const doc = document;
+  let array = doc.querySelectorAll("section");
+  let width = array[0].style.width;
+  let height = array[0].style.height;
+  for(let x = 0; x < array.length; x ++){
+    array[x].style.width = 0.75 * width;
+    array[x].style.height = 0.75 * height;
+  }
+}
 
 //Declare phoneSize() here
